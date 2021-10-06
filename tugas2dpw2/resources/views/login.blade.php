036cc30ede96e08b2e20f8f75817bca409e57e28
Elis Sulistia Sudrajat <91019520+elisslstia25@users.noreply.github.com>
2021-10-06 18:57:37 +0700

    Tugas 2

---
+++ tugas2dpw2/resources/views/login.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+     <section class="login">
+    	<h2>login Form<h2><hr>
+    	<form method="" action="">
+    	    <input type="text" name="username" placeholder="Inputkan Username..."> <br><br>
+    	    <input type="password" name="password" placeholder="Inputkan password..."> <br><br>
+    	    <input type="submit" name="submit" value="login">
+    	    <input type="reset" name="reset" value="Batal">
+    	    <p>Belum Memiliki Akun ? <a href="/registrasi">Daftar Disini</a></p>
+    	</form>
+    </section>
+</body>
+</html>
---
+++ tugas2dpw2/resources/views/registrasi.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registrasi form</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<section class="login">
+		<h2>Form Registrasi</h2><hr>
+		<form method="" action="">
+			<input type="text" name="nama" placeholder="Inputkan Nama Lengkap..."><br><br>
+			<textarea name="alamat" rows="5" placeholder="Inputkan Alamat..."></textarea> <br><br>
+			<input type="text" name="telpon" placeholder="Inputkan Nomor Hp..."><br><br>
+			<input type="email" name="email" placeholder="Inputkan email..."><br><br>
+			<input type="text" name="username" placeholder="Inputkan username..."><br><br>
+			<input type="password" name="password" placeholder="Inputkan password..."><br><br>
+			<input type="submit" name="submit" value="Daftar">
+			<input type="reset" name="reset" value="Batal">
+			<p><a href="/index">Kembali Ke Home</a></p>
+		</form>
+	</section>
+</body>
+</html>s
